6913af60c325dd92fa8bfe481b0fb1a74a7b2931
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2023-04-10 14:22:15 +0800

    delete function

--- cpp/App.cpp
+++ cpp/App.cpp
@@ -4,52 +4,6 @@
 
 using namespace std;
 
-void calculator()
-{
-    string supported_op = "+, -, *, /";
-    double first_number;
-    char op;
-    double second_number;
-    double result;
-
-    cout << "Enter first number:" << " ";
-    cin >> first_number;
-    cout << "Enter operator:" << " ";
-    cin >> op;
-    cout << "Enter second number:" << " ";
-    cin >> second_number;
-
-    if (op == '+' || op == '-' || op == '*' || op == '/')
-    {
-        if (op == '+')
-        {
-            result = first_number + second_number;
-        }
-        else if (op == '-')
-        {
-            result = first_number - second_number;
-        }
-        else if (op == '*')
-        {
-            result = first_number * second_number;
-        }
-        else if (op == '/')
-        {
-            result = first_number / second_number;
-        }
-
-        cout << "\n" << "result:" << " " << result << "\n" << endl;
-    }
-    else
-    {
-        cout << "\n" << endl;
-        cout << "Invalid or unsupported operator" << endl;
-        cout << "Supported operator:" << " " << supported_op << endl;
-        cout << "\n" << endl;
-    }
-
-}
-
 int main()
 {
 
@@ -96,9 +50,6 @@ int main()
         cout << "Supported operator:" << " " << supported_op << "\n" << endl;
     }
 
-    while (true)
-    {
-        calculator();
-    }
+
     return 0;
 }
